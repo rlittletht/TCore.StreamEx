87f08776ae4556cef4cc01c354607565a4c73847
Rob Little <rlittletht@users.noreply.github.com>
2018-09-30 20:52:09 -0700

    Buffers keep track of token start now

--- BufferedStreamEx.cs
+++ BufferedStreamEx.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 
 namespace TCore.StreamEx
 {
+    public class PinnedTokenExceedsBufferLength : Exception
+    {
+        public PinnedTokenExceedsBufferLength() { }
+        public PinnedTokenExceedsBufferLength(string msg) : base(msg) { }
+        public PinnedTokenExceedsBufferLength(string msg, Exception inner) : base(msg, inner) { }
+    }
+
+    public class SourceDataExhausted : Exception
+    {
+        public SourceDataExhausted() { }
+        public SourceDataExhausted(string msg) : base(msg) { }
+        public SourceDataExhausted(string msg, Exception inner) : base(msg, inner) { }
+    }
+
     public class BufferedStreamEx
     {
         private Stream m_stm;
-        private long m_ibStart;
-        private long m_ibLim;
+        private long m_ibFileStart;
+        private long m_ibFileLim;
         private SwapBuffer m_bufferMain;
         private SwapBuffer m_bufferSwap;
 
@@ -31,16 +45,16 @@ namespace TCore.StreamEx
             The file is opened Read only, allowing ReadWrite share opening (to allow
             the other thread to open the file read/write)
         ----------------------------------------------------------------------------*/
-        public BufferedStreamEx(string sFilename, long ibStart, long ibLim)
+        public BufferedStreamEx(string sFilename, long ibFileStart, long ibFileLim)
         {
             FileStream m_fs = new FileStream(sFilename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             m_stm = m_fs;
 
-            m_ibStart = ibStart;
-            m_ibLim = ibLim;
-            m_stm.Seek(m_ibStart, SeekOrigin.Begin);
-            m_bufferMain = new SwapBuffer(m_stm, ibLim);
-            m_bufferSwap = new SwapBuffer(m_stm, ibLim);
+            m_ibFileStart = ibFileStart;
+            m_ibFileLim = ibFileLim;
+            m_stm.Seek(m_ibFileStart, SeekOrigin.Begin);
+            m_bufferMain = new SwapBuffer(m_stm, ibFileLim);
+            m_bufferSwap = new SwapBuffer(m_stm, ibFileLim);
         }
 
         /*----------------------------------------------------------------------------
@@ -68,35 +82,104 @@ namespace TCore.StreamEx
         }
 
         /*----------------------------------------------------------------------------
-            %%Function: SwapBuffer
-            %%Qualified: TCore.ListenAz.Stage2.BufferedStreamEx.SwapBuffer
+            %%Function: SwapCurrentBuffer
+            %%Qualified: TCore.ListenAz.Stage2.BufferedStreamEx.SwapCurrentBuffer
             %%Contact: rlittle
 
             swap the current buffer with the other buffer (and fill the new buffer)
-            make sure we copy from ibPreserve to the end of the buffer to ensure we 
+            make sure we copy from pinned token start to the end of the buffer to ensure we 
             have a contiguous token
+
+            properly propagates (and resets) the token start offsets
+
+            the current seek position is also set properly to reflect the swap
         ----------------------------------------------------------------------------*/
-        bool SwapBuffer(int ibPreserve)
+        bool SwapCurrentBuffer()
         {
             int ibDest = 0;
 
-            if (ibPreserve != -1)
+            if (BufferCurrent.TokenStart != -1)
             {
                 // they are requesting that some portion of the current buffer
                 // be moved into the swap buffer when we read it -- this way
                 // we can have a token span a buffer boundary (though not be
                 // larger than a single buffer)
-                int ibCopy = ibPreserve;
+                int ibCopy = BufferCurrent.TokenStart;
                 while (ibCopy < BufferCurrent.Lim)
                     BufferOther.Bytes[ibDest++] = BufferCurrent.Bytes[ibCopy++];
+
+                // the current position will be set properly below (in FillBuffer)
             }
 
             if (!BufferOther.FillBuffer(ibDest))
                 return false;
 
+            if (BufferCurrent.TokenStart != -1)
+            {
+                // reset the tokens properly
+                BufferCurrent.ResetPinnedToken();
+                BufferOther.PinTokenStartAbsolute(0);
+            }
+
             m_fUseSwapBuffer = !m_fUseSwapBuffer;
 
             return true;
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: PinTokenStartRelative
+        	%%Qualified: TCore.StreamEx.BufferedStreamEx.PinTokenStartRelative
+        	%%Contact: rlittle
+        	
+            Pin the start of the token, using the given relative value.  (pass 0
+            if you want to pin the current position in the buffer. or -1 for the
+            previous character, etc)
+        ----------------------------------------------------------------------------*/
+        public void PinTokenStartRelative(int dib)
+        {
+            BufferCurrent.PinTokenStartRelative(dib);
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: ReadByte
+        	%%Qualified: TCore.StreamEx.BufferedStreamEx.ReadByte
+        	%%Contact: rlittle
+        	
+        ----------------------------------------------------------------------------*/
+        SwapBuffer.ReadByteBufferState ReadByte(out byte b)
+        {
+            SwapBuffer.ReadByteBufferState state = BufferCurrent.Read(out b);
+
+            if (BufferCurrent.NeedsFilled)
+            {
+                if (BufferCurrent.TokenStart == 0)
+                {
+                    return StreamEx.SwapBuffer.ReadByteBufferState.PinnedTokenExceedsBufferLength;
+
+                    // hmm, we have the entire buffer to ourselves, but no line ending was
+                    // found. just invent a break here
+                    // return Encoding.UTF8.GetString(BufferCurrent.Bytes, ibLineStart, ib - ibLineStart);
+                    // the next time they call ReadLine, it will fill the next buffer
+                }
+
+                if (!SwapCurrentBuffer())
+                {
+                    // couldn't fill the next buffer, so we are out of space...just return what we have
+                    return StreamEx.SwapBuffer.ReadByteBufferState.SourceDataExhausted;
+                    // return Encoding.UTF8.GetString(BufferCurrent.Bytes, ibLineStart, ib - ibLineStart);
+                }
+
+                // otherwise, we are good to go.
+                // the new buffer has all the stuff we already parsed between [ibLineStart and ib)
+                // so rebase them all such that ibLineStart is now 0
+
+                if (BufferCurrent.Cur >= BufferCurrent.Lim)
+                    throw new Exception("internal state failure");
+
+                return StreamEx.SwapBuffer.ReadByteBufferState.Succeeded;
+            }
+
+            return StreamEx.SwapBuffer.ReadByteBufferState.Succeeded;
         }
 
         /*----------------------------------------------------------------------------
@@ -110,50 +193,75 @@ namespace TCore.StreamEx
         ----------------------------------------------------------------------------*/
         public string ReadLine()
         {
-            if (BufferCurrent.Start >= BufferCurrent.Lim)
+            if (BufferCurrent.Cur >= BufferCurrent.Lim)
             {
-                if (!SwapBuffer(-1)) // nothing to preserve, just fill the buffer
+                if (!SwapCurrentBuffer()) // nothing to preserve, just fill the buffer
                     return null;
             }
 
             // start reading the line from the current position in the current buffer
-            int ib = BufferCurrent.Start;
-            int ibLineStart = ib;
+            
+            PinTokenStartRelative(0);
+            
             bool fLookingForLF = false;
 
             while (true)
             {
-                byte b = BufferCurrent.Bytes[ib];
+                // byte b = BufferCurrent.Bytes[ib];
+                byte b;
+
+                SwapBuffer.ReadByteBufferState state = ReadByte(out b);
+                // Read
 
                 if (b == 0x0a)
                 {
                     // we're done. If we were looking for it, great. if not, no matter, we're still done...
-                    BufferCurrent.SetPos(ib + 1);
+                    // BufferCurrent.SetPos(ib + 1);
+                    // post read, no change (consume the byte)
                     int cbLineEndingAdjust = fLookingForLF ? 1 : 0;
 
                     // remember we don't want the line ending as part of the string we construct. Since ib hasn't been adjusted
                     // for this character, the only thing we have to worry about is if there was a leading CR
-                    return Encoding.UTF8.GetString(BufferCurrent.Bytes, ibLineStart,
-                        ib - ibLineStart - cbLineEndingAdjust);
+                    return Encoding.UTF8.GetString(BufferCurrent.Bytes, BufferCurrent.TokenStart,
+                        BufferCurrent.Cur - BufferCurrent.TokenStart - cbLineEndingAdjust);
                 }
 
                 if (fLookingForLF)
                 {
                     // was looking for a matching LF, but didn't find. must be a naked LF
                     // push back this character (or rather,just don't eat it)
-                    BufferCurrent.SetPos(ib);
+                    // BufferCurrent.SetPos(ib);
+                    BufferCurrent.Unget();
+                    // post read, push back this character (seek back by 1)
 
                     // remember to chop off the LF
-                    return Encoding.UTF8.GetString(BufferCurrent.Bytes, ibLineStart, ib - ibLineStart - 1);
+                    return Encoding.UTF8.GetString(BufferCurrent.Bytes, BufferCurrent.TokenStart, BufferCurrent.Cur - BufferCurrent.TokenStart - 1);
                 }
 
                 if (b == 0x0d)
                 {
                     fLookingForLF = true;
                 }
-                // otherwise, keep going forward
 
+                // otherwise, keep going forward
+                if (state == StreamEx.SwapBuffer.ReadByteBufferState.PinnedTokenExceedsBufferLength
+                    || state == StreamEx.SwapBuffer.ReadByteBufferState.SourceDataExhausted)
+                {
+                    // hmm, if we got PinnedTokenExceedsBufferLength, then we have the entire buffer
+                    // to ourselves, but no line ending was found. just invent a break here
+
+                    // if we got SourceDataExhausted, then we couldn't fill the next buffer, so we are
+                    // out of space...just return what we have
+
+                    // in either case , we do the same thing (and if we are beyond the end of the buffer
+                    // the next read will fill the buffer for us)
+                    return Encoding.UTF8.GetString(BufferCurrent.Bytes, BufferCurrent.TokenStart, BufferCurrent.Cur - BufferCurrent.TokenStart);
+                }
+
+#if OLD
                 ib++;
+
+                // all this below should be handled in the read.
                 if (ib >= BufferCurrent.Lim)
                 {
                     if (ibLineStart == 0)
@@ -164,7 +272,7 @@ namespace TCore.StreamEx
                         // the next time they call ReadLine, it will fill the next buffer
                     }
 
-                    if (!SwapBuffer(ibLineStart))
+                    if (!SwapCurrentBuffer(ibLineStart))
                     {
                         // couldn't fill the next buffer, so we are out of space...just return what we have
                         return Encoding.UTF8.GetString(BufferCurrent.Bytes, ibLineStart, ib - ibLineStart);
@@ -178,7 +286,8 @@ namespace TCore.StreamEx
 
                     if (ib >= BufferCurrent.Lim)
                         throw new Exception("internal state failure");
-                }
+#endif
+            
             }
         }
 
--- SwapBuffer.cs
+++ SwapBuffer.cs
@@ -13,14 +13,23 @@ namespace TCore.StreamEx
     public class SwapBuffer
     {
         private byte[] m_rgb = new byte[1024];
-        private int m_ibBufferStart = -1;
+        private int m_ibBufferCur = -1;
         private int m_ibBufferLim = -1;
         private readonly long m_ibFileLim;
         private readonly Stream m_stm;
 
-        public int Start => m_ibBufferStart;
+
+        // this is the pinned seek position for tokens -- if we need to switch
+        // to a new buffer, we will ensure that this token is moved into the new
+        // buffer (to keep our token together).  This means that tokens are restricted
+        // to the buffer size
+        private int m_ibTokenStart = -1;
+
+        public int TokenStart => m_ibTokenStart;
+        public int Cur => m_ibBufferCur;
         public int Lim => m_ibBufferLim;
         public byte[] Bytes => m_rgb;
+
 
         /*----------------------------------------------------------------------------
             %%Function: SwapBuffer
@@ -59,10 +68,84 @@ namespace TCore.StreamEx
             if (cbRead != cbToRead)
                 throw new Exception("read failure");
 
-            m_ibBufferStart = ibStart;
+            m_ibBufferCur = ibStart;
             m_ibBufferLim = ibStart + cbRead;
 
             return true;
+        }
+
+        public void Unget()
+        {
+            if (m_ibBufferCur == 0)
+                throw new Exception("cannot unget the start of the buffer");
+
+            m_ibBufferCur--;
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: PinTokenStartRelative
+        	%%Qualified: TCore.StreamEx.SwapBuffer.PinTokenStartRelative
+        	%%Contact: rlittle
+        	
+            record where in this buffer we would like to pin as the start of a token
+
+            if we have to fill a new buffer, this token will prefill the new buffer
+            to ensure the token doesn't span a buffer
+        ----------------------------------------------------------------------------*/
+        public void PinTokenStartRelative(int dib)
+        {
+            m_ibTokenStart = m_ibBufferCur + dib;
+            if (m_ibTokenStart < 0 || m_ibTokenStart >= m_ibBufferLim)
+                throw new Exception("pinned token start outside of buffer");
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: PinTokenStartAbsolute
+        	%%Qualified: TCore.StreamEx.SwapBuffer.PinTokenStartAbsolute
+        	%%Contact: rlittle
+        	
+        ----------------------------------------------------------------------------*/
+        public void PinTokenStartAbsolute(int ib)
+        {
+            m_ibTokenStart = ib;
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: ResetPinnedToken
+        	%%Qualified: TCore.StreamEx.SwapBuffer.ResetPinnedToken
+        	%%Contact: rlittle
+        	
+        ----------------------------------------------------------------------------*/
+        public void ResetPinnedToken()
+        {
+            m_ibTokenStart = -1;
+        }
+
+        public bool NeedsFilled => m_ibBufferCur >= m_ibBufferLim;
+        public bool HasPinnedToken => m_ibTokenStart != -1;
+        
+        public enum ReadByteBufferState
+        {
+            Succeeded,
+            PinnedTokenExceedsBufferLength,
+            SourceDataExhausted
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: Read
+        	%%Qualified: TCore.StreamEx.SwapBuffer.Read
+        	%%Contact: rlittle
+        	
+        ----------------------------------------------------------------------------*/
+        public ReadByteBufferState Read(out byte b)
+        {
+            b = 0;
+
+            if (m_ibBufferCur >= m_ibBufferLim)
+                return ReadByteBufferState.SourceDataExhausted;
+
+            b = Bytes[m_ibBufferCur++];
+            return ReadByteBufferState.Succeeded;
         }
 
         /*----------------------------------------------------------------------------
@@ -74,7 +157,9 @@ namespace TCore.StreamEx
         ----------------------------------------------------------------------------*/
         public void SetPos(int ib)
         {
-            m_ibBufferStart = ib;
+            m_ibBufferCur = ib;
         }
+
+        
     }
 }
